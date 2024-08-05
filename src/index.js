34bf34adf17f35f6ba2291f8e25e69588ccdb814
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-08-05 10:47:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 
-// You should implement your task here.
+module.exports = function towelSort(matrix) {
 
-module.exports = function towelSort (matrix) {
-  return [];
+  const rez = []
+
+  if (!matrix) { return rez }
+
+  matrix.map((el, i) => {
+    if (i % 2) {
+      el.reverse().forEach(elem => {
+        rez.push(elem)
+      })
+    } else {
+      el.forEach(elem => {
+        rez.push(elem)
+      })
+    }
+  })
+
+  return rez
+
 }
+
+// const testArray = [
+//   [1, 2, 4],
+//   [5, 6, 7, 8],
+//   [9, 12],
+// ]
+
+// function towel(arr) {
+//   const rez = []
+
+//   arr.map((el, i) => {
+//     if (i % 2) {
+//       el.reverse().forEach(elem => {
+//         rez.push(elem)
+//       })
+//     } else {
+//       el.forEach(elem => {
+//         rez.push(elem)
+//       })
+//     }
+//   })
+
+//   return rez
+// }
+
+// console.log(towel(testArray)); //[1, 2, 4, 8, 7, 6, 5, 9, 12]
